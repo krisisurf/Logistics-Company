e95f762e15b90e15b66757488ed786fd417a68b2
boromir-p <61236415+boromir-p@users.noreply.github.com>
2022-07-18 11:16:19 +0300

    Update OfficeService.java

--- src/main/java/com/bosch/logistics/service/OfficeService.java
+++ src/main/java/com/bosch/logistics/service/OfficeService.java
@@ -8,6 +8,8 @@ public interface OfficeService {
     List<Office> getOffices();
     Office getOffice(long id);
     Office create(Office office);
+    Set<Product> productsInOffice(long officeId);
+    int productsInOfficeCount(long officeId);
     Office update(Office office, long id);
     void delete(long id);
 }
