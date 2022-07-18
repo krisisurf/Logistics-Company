8bb42eebd6add22e58f62287f27a60111a066544
boromir-p <61236415+boromir-p@users.noreply.github.com>
2022-07-18 11:19:53 +0300

    Update CustomerServiceImpl.java

--- src/main/java/com/bosch/logistics/service/implementation/CustomerServiceImpl.java
+++ src/main/java/com/bosch/logistics/service/implementation/CustomerServiceImpl.java
@@ -42,6 +42,10 @@ public class CustomerServiceImpl implements CustomerService {
         customerRepository.deleteById(id);
     }
 
+
+    public List<Customer> findAllByFirstNameStartsWithAndLastNameStartsWithOrderByTelAsc(String startFirstName, String startLastName) {
+        return customerRepository.findAllByFirstNameStartsWithAndLastNameStartsWithOrderByTelAsc(startFirstName, startLastName);
+        }
     @Override
     public List<Customer> findByFirstNameContainingAndTelEndingWith(String fname, String phone) {
         return customerRepository.findByFirstNameContainingAndTelEndingWith(fname, phone);
