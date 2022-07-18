ca52be13538628544470211b2b20060c7246ce1c
boromir-p <boromir.d.petrov@gmail.com>
2022-07-18 12:08:02 +0300

    Find product by telephone and status functionality.

--- src/main/java/com/bosch/logistics/service/implementation/AddressServiceImpl.java
+++ src/main/java/com/bosch/logistics/service/implementation/AddressServiceImpl.java
@@ -18,6 +18,7 @@ public class AddressServiceImpl implements AddressService {
 
     public List<Address> findAllAddressesByCityOrCode(String city, String code) {
         return repo.findAllAddressesByCityOrCode(city, code);
+    }
 
     @Override
     public List<Address> getAddresses() {
