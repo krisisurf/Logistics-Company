03d6146e2d212f5ad26b8465199fc4c5d6c91eab
krisisurf <61236255+krisisurf@users.noreply.github.com>
2022-07-18 11:55:30 +0300

    Update ProductService.java

--- src/main/java/com/bosch/logistics/service/ProductService.java
+++ src/main/java/com/bosch/logistics/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.bosch.logistics.service;
 
+import com.bosch.logistics.entity.Address;
 import com.bosch.logistics.entity.Product;
+import com.bosch.logistics.entity.ProductStatus;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 public interface ProductService {
     List<Product> getProducts();
