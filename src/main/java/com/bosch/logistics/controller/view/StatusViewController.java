4b184c660c5f0fde83000764f31caa0fa63b4294
krisisurf <krisisurf@gmail.com>
2022-07-19 12:55:29 +0300

    update delete add status

--- src/main/java/com/bosch/logistics/controller/view/StatusViewController.java
+++ src/main/java/com/bosch/logistics/controller/view/StatusViewController.java
@@ -1,10 +1,10 @@
 package com.bosch.logistics.controller.view;
 
+import com.bosch.logistics.entity.ProductStatus;
 import com.bosch.logistics.service.StatusService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/status")
@@ -21,5 +21,37 @@ public class StatusViewController {
         model.addAttribute("statuses", statusService.getStatuses());
         return "/status/status";
     }
+
+    @GetMapping("/create-status")
+    public String showCreateStatusForm(Model model) {
+        model.addAttribute("productStatus", new ProductStatus());
+        return "/status/create-status";
+    }
+
+    @PostMapping("/create")
+    public String createStatus(@ModelAttribute ProductStatus status) {
+        statusService.create(status);
+        return "redirect:/status";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditStatusForm(Model model, @PathVariable long id) {
+        model.addAttribute("productStatus", statusService.getStatus(id));
+        return "/status/edit-status";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateStatusForm(@PathVariable long id, ProductStatus productStatus) {
+        statusService.update(productStatus, id);
+        return "redirect:/status";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteStatus(@PathVariable long id) {
+        statusService.delete(id);
+
+        return "redirect:/status";
+    }
+
 
 }
