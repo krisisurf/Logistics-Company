5edaf4fd7ae134764bdfe78bad3d7685db4bb5fb
krisisurf <krisisurf@gmail.com>
2022-07-29 12:07:22 +0300

    cleaning code

--- src/main/java/com/bosch/logistics/controller/view/StatisticsViewController.java
+++ src/main/java/com/bosch/logistics/controller/view/StatisticsViewController.java
@@ -7,7 +7,9 @@ import com.bosch.logistics.entity.User;
 import com.bosch.logistics.service.CustomerService;
 import com.bosch.logistics.service.StatusService;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,48 +21,50 @@ import java.util.Set;
 @RequestMapping("/statistics")
 public class StatisticsViewController {
     private CustomerService customerService;
+    private Set<String> methods;
 
-    Set<String> methods = new HashSet<String>();
     public StatisticsViewController(CustomerService customerService) {
         this.customerService = customerService;
+        methods = new HashSet<>();
         methods.add("getTotalWeightAllProducts");
     }
 
-
-
     @GetMapping("/method")
-    public String createStatus(Model model, Customer customer, String method) {
-        if(method.equals("getTotalWeightAllProducts")) {
+    public String showMethodForm(Model model, Customer customer, String method) {
+        if (method.equals("getTotalWeightAllProducts")) {
             model.addAttribute("weight", customerService.getTotalWeightAllProducts(customer.getId()));
             return "/statistics/totalweight-statistics";
         }
-        else{
-            return "/notfound";
-        }
+
+        return "/notfound";
     }
 
     @GetMapping
-    public String productView(Model model) {
+    public String showMethodsListForm(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
 
-        Customer customer = null;
+        Customer customer;
 
-        if(user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("CUSTOMER"))) {
-            customer = customerService.getCustomer(user.getId());
+        for (GrantedAuthority a : user.getAuthorities()) {
+            if (a.getAuthority().equals("CUSTOMER")) {
+                customer = customerService.getCustomer(user.getId());
+                model.addAttribute("weight", customerService.getTotalWeightAllProducts(customer.getId()));
+                return "/statistics/statistics";
 
-            model.addAttribute("weight", customerService.getTotalWeightAllProducts(customer.getId()));
+            } else if (a.getAuthority().equals("ADMIN") || a.getAuthority().equals("OFFICE_EMPLOYEE")
+                    || a.getAuthority().equals("DRIVER")) {
 
-            return "/statistics/statistics";
+                model.addAttribute("customers", customerService.getCustomers());
+                model.addAttribute("customer", new Customer());
+
+                model.addAttribute("methods", methods);
+                model.addAttribute("method", new String());
+                return "statistics/choose-statistics";
+            }
         }
-        else{
-            model.addAttribute("customers",customerService.getCustomers());
-            model.addAttribute("customer", new Customer());
 
-            model.addAttribute("methods",methods);
-            model.addAttribute("method", new String());
-            return "statistics/choose-statistics";
-        }
+        return "/notfound";
     }
 
 }
